24ab1971c93377d83e328d80eaa4526dfa4f1701
pyatizbyantsevia <ilya_2.0@mail.ru>
2024-02-05 09:20:55 +0000

    Export all objects
    Signed-off-by: pyatizbyantsevia <ilya_2.0@mail.ru>

--- api/generated/python/dormyboba_api/__init__.py
+++ api/generated/python/dormyboba_api/__init__.py
@@ -1,6 +1,5 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent))
-print('\n'.join(sys.path)) ###
-import v1api_pb2
-import v1api_pb2_grpc
+from v1api_pb2 import *
+from v1api_pb2_grpc import *

f9f258159079689c3bc2ff5d842c0db43de34823
pyatizbyantsevia <ilya_2.0@mail.ru>
2024-02-05 09:08:15 +0000

    Logging
    Signed-off-by: pyatizbyantsevia <ilya_2.0@mail.ru>

--- api/generated/python/dormyboba_api/__init__.py
+++ api/generated/python/dormyboba_api/__init__.py
@@ -1,4 +1,6 @@
 import sys
-sys.path.append(".")
+from pathlib import Path
+sys.path.append(Path(__file__).resolve().parent.parent)
+print('\n'.join(sys.path)) ###
 import v1api_pb2
 import v1api_pb2_grpc
